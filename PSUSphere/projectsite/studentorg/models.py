adcfb18d3a5db0c1281036bfcdecb0d2479c2e6b
Pandasaez <jasperram3489@gmail.com>
2025-03-06 09:14:25 +0800

    Models and Adming Working

--- PSUSphere/projectsite/studentorg/models.py
+++ PSUSphere/projectsite/studentorg/models.py
@@ -1,46 +1,46 @@
 from django.db import models
 
 # Create your models here.
-class BaseModel (model.Model):
-    created_at = model.DateTimeField(auto_now_add=True, db_indext=True)
-    upadted_at = = model.DateTimeField(auto_now=True)
+class BaseModel (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
 class College(BaseModel):
-    collage_name = model.Charfield(max_length=150)
+    collage_name = models.CharField(max_length=150)
 
-    def_str_(self):
+    def __str__ (self):
         return self.college_name
 
-class Program(Basemodel):
-    prog_name = model.Charfield(max_length=150)
+class Program(BaseModel):
+    prog_name = models.CharField(max_length=150)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
-    def_str_(self):
+    def _str_(self):
         return self.prog_name
 
 class Organization (BaseModel):
     name = models.CharField(max_length=250)
     college = models. ForeignKey(
-        College, null=True, blank-True, on_delete models.CASCADE)
+        College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
 
-    def_str_(self):
+    def _str_(self):
         return self.name
 
 class Student(BaseModel):
-    student_id= models. CharField(max_length=15)
-    lastname models.CharField(max_length=25)
-    firstname models.CharField(max_length=25)
-    middlename models.CharField(max_length=25, blank True, null-True)
-    program models.ForeignKey (Program, on_delete models.CASCADE)
+    student_id = models. CharField(max_length=15)
+    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    middlename = models.CharField(max_length=25, blank=True, null=True)
+    program = models.ForeignKey (Program, on_delete=models.CASCADE)
 
-    def_str_(self):
+    def _str_(self):
         return f"(self.lastname), (self.firstname)"
 
 class OrgMember(BaseModel):
-    student = models. ForeignKey (Student, on delete models.CASCADE)
-    organization = models. ForeignKey(Organization, on_delete models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    organization = models. ForeignKey(Organization, on_delete=models.CASCADE)
     data_joined = models. DateField()
