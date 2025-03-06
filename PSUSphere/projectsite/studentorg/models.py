c4583e10e4e39ea48d25ac00753bd1482c32f1ed
Pandasaez <jasperram3489@gmail.com>
2025-03-06 08:51:33 +0800

    Code1

--- PSUSphere/projectsite/studentorg/models.py
+++ PSUSphere/projectsite/studentorg/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel (model.Model):
+    created_at = model.DateTimeField(auto_now_add=True, db_indext=True)
+    upadted_at = = model.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class College(BaseModel):
+    collage_name = model.Charfield(max_length=150)
+
+    def_str_(self):
+        return self.college_name
+
+class Program(Basemodel):
+    prog_name = model.Charfield(max_length=150)
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
+
+    def_str_(self):
+        return self.prog_name
+
