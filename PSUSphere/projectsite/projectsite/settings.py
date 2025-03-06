183f3c99f192021708b4de6af456333a01b21c75
CaidoZur <officialzurgmon7@gmail.com>
2025-03-06 08:34:01 +0800

    fixing again

--- PSUSphere/projectsite/projectsite/settings.py
+++ PSUSphere/projectsite/projectsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aqh@vcp@-cunvn++qj@a6n$*8d@b53)xfj993(xm@8wjsq7r$n'
+SECRET_KEY = 'django-insecure-i6^%e2ko+q2-31%@ms+s(w!yo-cvu5relocp-r$rs1ycf71216'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
