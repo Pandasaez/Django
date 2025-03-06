afbbbdf45b423321adc3d1876096b32b35b7f8d7
CaidoZur <officialzurgmon7@gmail.com>
2025-03-06 08:52:39 +0800

    Code2

--- PSUSphere/projectsite/studentorg/models.py
+++ PSUSphere/projectsite/studentorg/models.py
@@ -21,3 +21,26 @@ class Program(Basemodel):
     def_str_(self):
         return self.prog_name
 
+class Organization (BaseModel):
+    name = models.CharField(max_length=250)
+    college = models. ForeignKey(
+        College, null=True, blank-True, on_delete models.CASCADE)
+    description = models.CharField(max_length=500)
+
+    def_str_(self):
+        return self.name
+
+class Student(BaseModel):
+    student_id= models. CharField(max_length=15)
+    lastname models.CharField(max_length=25)
+    firstname models.CharField(max_length=25)
+    middlename models.CharField(max_length=25, blank True, null-True)
+    program models.ForeignKey (Program, on_delete models.CASCADE)
+
+    def_str_(self):
+        return f"(self.lastname), (self.firstname)"
+
+class OrgMember(BaseModel):
+    student = models. ForeignKey (Student, on delete models.CASCADE)
+    organization = models. ForeignKey(Organization, on_delete models.CASCADE)
+    data_joined = models. DateField()
