814047263d90d329fc31da5409ec115015eb390c
Panda <115606194+Pandasaez@users.noreply.github.com>
2025-03-29 01:15:57 +0800

    organization

--- PSUSphere/projectsite/studentorg/views.py
+++ PSUSphere/projectsite/studentorg/views.py
@@ -6,6 +6,13 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+    
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5
+
 
 
 
