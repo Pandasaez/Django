9c3a485d1c2ca35fb47d9b4c0d9936785254e636
Zuriel Montallana <officialzurgmon7@gmail.com>
2025-03-30 14:18:39 +0800

    Created login and logout, Modify views, urls, settings, base

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views 
+from django.contrib.auth import views as auth_views
 from studentorg.views import OrganizationMemberList, OrganizationMemberCreateView, OrganizationMemberUpdateView, OrganizationMemberDeleteView
 from studentorg.views import StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView
 from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView
@@ -51,4 +52,7 @@ urlpatterns = [
     path('program_list/add', ProgramCreateView.as_view(), name='program-add'),  
     path('program_list/<pk>', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<pk>/delete', ProgramDeleteView.as_view(), name='program-delete'),
+
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
